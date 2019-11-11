b33124fbc28fde1b2dc4a189731ff48f1ac813ea
Mia Dugas <miadugas@outlook.com>
2019-11-11 11:17:26 -0700

    music-player-app

---
+++ script.js
@@ -0,0 +1,30 @@
+const songs =[
+"Anger.mp3",
+"Chameleon.mp3",
+"Crystal Blue.mp3",
+"I Can't Breathe.mp3",
+"I Was Wrong.mp3",
+"Just A Glance.mp3",
+"Lie To Me.mp3",
+"Never Alone.mp3",
+"One for the Road.mp3",
+"Party's Back.mp3",
+"Shell Shocked.mp3",
+"Wade It Out.mp3",
+"When We Let Go.mp3",
+]
+
+const createSongList = () =>{
+const list = document.createElement ("ol")
+
+for(let i = 0; i <songs.length; i++) {
+const item = document.createElement("li")
+item.appendChild(document.createTextNode(songs[i]))
+
+list.appendChild(item)
+
+}
+return list
+}
+
+document.getElementById("songList").appendChild(createSongList())
